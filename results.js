fffcdd327dc2107fc2dbb95cc3b3f1267e689ecf
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-12-07 09:26:36 +0300

    Update results.js

--- results.js
+++ results.js
@@ -113,9 +113,9 @@ async function get_data() {
 }
 
 function draw_cohorts(cohorts) {
-  var result = '<table class="table table-sm"><tr><th style="border: 0" rowspan="2" colspan="2"></th><th colspan="6" style="text-align: center;">Участвуют, дней подряд</th></tr>';
-  const map = {0: 'Сегодня'};
   const max_cols = cohorts[0].length + 1;
+  var result = `<table class="table table-sm"><tr><th style="border: 0" rowspan="2" colspan="2"></th><th colspan="${max_cols}" style="text-align: center;">Участвуют, дней подряд</th></tr>`;
+  const map = {0: 'Сегодня'};
   for (let j = 0; j < max_cols - 1; j++) {
     if (j == -1)
       result += `<th></th>`;
